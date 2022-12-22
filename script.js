72d82ee9b836b2c42efc0a6bc6676707e7c1e086
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-22 23:13:39 +0530

    backdrop

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
 
 const main = document.getElementById("main");
+const backdrop = document.getElementById("backdrop");
 const span = document.getElementById("span");
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const cross = document.querySelector(".cross");
 const navbar = document.querySelector(".navbar");
+const rightArrow = document.querySelector(".right-arrow");
+const leftArrow = document.querySelector(".left-arrow");
+let index = 1;
 
 getMovies(API_URL);
 
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.results);
+  backdropImages(data.results, index);
   showMovies(data.results);
 }
 
@@ -29,8 +33,13 @@ async function searchMovies(url) {
 function showMovies(moviesData) {
   main.innerHTML = "";
   moviesData.forEach((movie) => {
-    const { original_title, overview, poster_path, vote_average } = movie;
-    console.log(original_title, overview);
+    const {
+      original_title,
+      overview,
+      poster_path,
+      vote_average,
+      backdrop_path,
+    } = movie;
 
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie-container");
@@ -53,27 +62,47 @@ function showMovies(moviesData) {
   });
 }
 
-function getColor(vote){
-    if(vote>8){
-        return 'green';
-    }
-    else if(vote>5){
-        return 'blue';
-    }
-    else{
-        return 'red';
-    }
+function backdropImages(moviesData, i) {
+  moviesData.forEach((movie) => {
+    const { backdrop_path } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add(`backdrop-container-${i}`);
+
+    movieEl.innerHTML = `
+        <div id="backdrop-image" class="movie">
+        <img src="${IMG_PATH + backdrop_path}" alt="">
+        </div>
+        `;
+
+    showbackdrop(i, movieEl);
+    i++;
+  });
+}
+
+function showbackdrop(index, movieEl) {
+  backdrop.classList.contains(`backdrop-container-${index}`);
+  backdrop.appendChild(movieEl);
+}
+
+function getColor(vote) {
+  if (vote > 8) {
+    return "green";
+  } else if (vote > 5) {
+    return "blue";
+  } else {
+    return "red";
+  }
 }
 
 // form
-form.addEventListener('submit', function(e){
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(SEARCHAPI + input.value)
+  console.log(SEARCHAPI + input.value);
   searchMovies(SEARCHAPI + input.value);
-})
-
+});
 
 // Navbar
-cross.addEventListener('click', function(){
-  navbar.classList.toggle('hidden')
-})
+cross.addEventListener("click", function () {
+  navbar.classList.toggle("hidden");
+});
